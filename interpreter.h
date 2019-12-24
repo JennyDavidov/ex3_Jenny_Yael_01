fc5ce45a6a8f31f4dacc40e9acc6bf2f15841a10
yaelb6 <yaelberman6@gmail.com>
2019-12-24 15:27:05 +0200

    xml reading

---
+++ interpreter.h
@@ -0,0 +1,32 @@
+//
+// Created by yael6 on 13/11/2019.
+//
+
+#ifndef EX1_INTERPRETER_H
+#define EX1_INTERPRETER_H
+
+#include "ex1.h"
+#include "Expression.h"
+#include <string>
+#include <map>
+#include <stack>
+#include <queue>
+using namespace std;
+
+class Interpreter {
+    map<string,string> variables;
+public:
+    Interpreter();
+    virtual ~Interpreter();
+    void setVariables(string str);
+    Expression* interpret(string string);
+
+private:
+    bool isEqualOp(stack<string> s, char op);
+    bool isHighOp(stack<string> s, char op);
+    Expression* anotherExp(queue<string> &output, stack<double> &nums, Expression* ex1);
+};
+
+
+
+#endif //EX1_INTERPRETER_H
