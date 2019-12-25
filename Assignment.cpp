0be7d04149d43b8f1f3096c6453be5ed03c06c21
Jenny Davidov <you@example.com>
2019-12-25 16:42:37 +0200

    Ass

---
+++ Assignment.cpp
@@ -0,0 +1,21 @@
+//
+// Created by jenny on 25/12/2019.
+//
+
+#include "Assignment.h"
+#include "Sleep.h"
+#include <string>
+using namespace std;
+
+
+int Assignment::execute(string *str, Interpreter *interpreter) {
+//    if (str->find_first_of("+-/*") != string::npos) {
+//        Expression *ex = interpreter->interpret(*str);
+//         = ex->calculate();
+//    } else {
+//         = stod(*str);
+//    }
+
+}
+
+Assignment::Assignment() {}
